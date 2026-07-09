df4b1184a929751fc94bb9027ba76db9f1c19af5
alexander-mm <alexanderwolftrade@gmail.com>
2026-07-09 15:13:24 -0500

    feat(repository): complete RoleRepository read methods

--- server/src/repositories/RoleRepository.ts
+++ server/src/repositories/RoleRepository.ts
@@ -5,19 +5,43 @@ import { BaseRepository } from "./base/BaseRepository.js";
 export class RoleRepository extends BaseRepository {
 
     async findAll(): Promise<Role[]> {
-
         return this.prisma.role.findMany({
-
             where: {
                 isActive: true
             },
-
             orderBy: {
                 name: "asc"
             }
-
         });
-
     }
+    
+
+    async findById(id: bigint): Promise<Role | null> {
+        return this.prisma.role.findUnique({
+            where: {
+                id
+            }
+        })
+    }
+
+
+    async findByUuid(uuid: string): Promise<Role | null> {
+        return this.prisma.role.findUnique({
+            where: {
+                uuid
+            }
+        });
+    }
+
+    async exists(id: bigint): Promise<boolean> {
+        const count = await this.prisma.role.count({
+            where: {
+                id,
+                isActive: true
+            }
+        });
+        return count > 0;
+    }
+
 }
 
