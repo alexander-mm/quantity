c5522423768202e27cacb54e9a7cf7099492a1ae
alexander-mm <alexanderwolftrade@gmail.com>
2026-08-24 16:31:12 -0500

    fix_credit_accounts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -41,6 +41,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,svg,png,ico,woff,woff2}"],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
         navigateFallbackDenylist: [/^\/api/]
       }
     }),
